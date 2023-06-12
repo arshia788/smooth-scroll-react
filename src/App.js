eecb24367984efa9bca121afd500dc68a6dd3978
Amirali Amanipour <arshia.ala80@gmail.com>
2023-06-12 22:55:39 +0330

    final project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+
+import { FaArrowUp } from "react-icons/fa";
+
 
 function App() {
+
+  const [show, setShow]= useState(false);
+
+  useEffect(()=>{
+    window.addEventListener('scroll',()=>{
+      if(window.pageYOffset > 400){
+        setShow(true)
+      }else{
+        setShow(false)
+      }
+    })
+  },[show])
+
+
+  const scrollTop=()=>{
+    window.scroll({top:0, behavior:'smooth'}) 
+  }
+
   return (
     <div>
-      hello there
+      <section id='a'>first</section>
+      <section id='b'>second</section>
+      <section id='c'>third</section>
+      <section id='d'>fourth</section>
+
+      <button  
+      onClick={scrollTop}
+      className={`${show ?'to-top active' :'to-top'}`}>
+        <FaArrowUp />
+      </button>
+      
     </div>
   )
 }
